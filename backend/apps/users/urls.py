356860037a42a235d3ad95b06d2e189b6786eb25
buchii <oko22007@byui.edu>
2025-03-28 09:17:47 +0100

    feat: update urls

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('auth/register/', views.register_user, name='create-user'),
     path('auth/signin/', views.signin, name='sign-in'),
     path('auth/sigin_google/', views.google_login_view, name='google_sign_in'),
+    path("user/profile/", views.UserProfileView.as_view(), name="user-profile"),
 
 ]
