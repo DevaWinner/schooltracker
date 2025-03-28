ae96438931f04e53c8a114c98fc7510d0e29031a
wallacejoe <JAWcoder@gmail.com>
2025-03-28 13:39:11 -0700

    Created infrastructure for integration

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -7,6 +7,6 @@ app_name = 'users'
 urlpatterns = [
     path('auth/register/', views.register_user, name='create-user'),
     path('auth/signin/', views.signin, name='sign-in'),
-    path('auth/sigin_google/', views.google_login_view, name='google_sign_in'),
+    path('auth/signin_google/', views.google_login_view, name='google_sign_in'),
 
 ]
