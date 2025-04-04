00170b87e29944d28f3e35793bb7c29f33b388a8
Aniekan Winner Anietie <winneraaniekan@gmail.com>
2025-04-04 18:53:47 +0100

    Remove Whitenoise from requirements and clean up ASGI config

--- backend/schooltracker_backend/asgi.py
+++ backend/schooltracker_backend/asgi.py
@@ -1,16 +1,9 @@
-"""
-ASGI config for schooltracker_backend project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'schooltracker_backend.settings')
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "schooltracker_backend.settings")
 application = get_asgi_application()
+
+# Export the ASGI application as "handler" and "app"
+handler = application
+app = application
