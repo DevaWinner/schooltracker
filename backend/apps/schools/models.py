0821eb2ed90d754970f9d35318ab96561cc0d2c7
buchii <oko22007@byui.edu>
2025-04-04 22:09:08 +0100

    feat: create the schools directory model

--- backend/apps/schools/models.py
+++ backend/apps/schools/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class School(models.Model):
+    name = models.CharField(max_length=200)
+    country = models.CharField(max_length=100)
+    state_province = models.CharField(max_length=100, null=True, blank=True)
+    website = models.URLField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
