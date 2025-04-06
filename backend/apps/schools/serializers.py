8c76150e412b9da45c6f751e5d7120738eb2935a
buchii <oko22007@byui.edu>
2025-04-06 02:03:03 +0100

    feat: create serializers for the school directory

--- backend/apps/schools/serializers.py
+++ backend/apps/schools/serializers.py
@@ -3,11 +3,6 @@ from rest_framework import serializers
 from .models import School
 
 class SchoolSerializer(serializers.ModelSerializer):
-    name = serializers.CharField(required=False, allow_null=True)
-    country = serializers.CharField(required=False, allow_null=True)
-    website = serializers.CharField(required=False, allow_null=True)
-    state_province = serializers.CharField(required=False, allow_null=True)
-    
     class Meta:
         model = School
         fields = '__all__'
