ab9a58376cb054bd5240626e903311a0813c18fc
buchii <oko22007@byui.edu>
2025-04-08 17:19:13 +0100

    feat: add new urls (endpoints) to match the new schools views

--- backend/apps/schools/urls.py
+++ backend/apps/schools/urls.py
@@ -5,6 +5,8 @@ from apps.schools import views
 app_name = 'schools'
 
 urlpatterns = [
-    path('', views.SchoolListCreateAPIView.as_view(), name='schools-directory'),
-    path('<int:pk>/', views.SchoolDetailAPIView.as_view(), name='school-detail'),
+    path('schools/', views.SchoolListCreateAPIView.as_view(), name='schools-directory'),
+    path('schools/<int:pk>/', views.SchoolDetailAPIView.as_view(), name='school-detail'),
+    path('institutions/', views.InstitutionListAPIView.as_view(), name='institution-list'),
+    path('institutions/<int:id>/', views.InstitutionDetailAPIView.as_view(), name='institution-detail'),
 ]
