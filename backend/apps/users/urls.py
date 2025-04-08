9a32e6c6334af44f4c65fd33465371d7825fcdf8
buchii <oko22007@byui.edu>
2025-04-08 17:17:25 +0100

    feat: modify the user url endpoints

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('auth/register/', views.register_user, name='create-user'),
     path('auth/signin/', views.signin, name='sign-in'),
     path('auth/sigin_google/', views.google_login_view, name='google_sign_in'),
-    path("user/<int:user_id>/profile/", views.UserProfileView.as_view(), name="user-profile"),
-    path('user/<int:user_id>/info/', views.UserInfoView.as_view(), name='user-info'),
-    path('user/<int:user_id>/settings/', views.UserSettingsView.as_view(), name='user-settings'),
+    path("user/profile/", views.UserProfileView.as_view(), name="user-profile"),
+    path('user/info/', views.UserInfoView.as_view(), name='user-info'),
+    path('user/settings/', views.UserSettingsView.as_view(), name='user-settings'),
 ]
