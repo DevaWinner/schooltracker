068d6da260c5b2226559f1e668f37a7486fc50a2
buchii <oko22007@byui.edu>
2025-04-15 23:27:51 +0100

    remove backend/ dir

--- backend/apps/schools/utils/custom_pagination.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.pagination import PageNumberPagination
-
-# Custom Pagination Class
-class CustomPagination(PageNumberPagination):
-    page_size = 10  # Default number of items per page
-    page_size_query_param = 'page_size'
-    max_page_size = 100  # Max limit
